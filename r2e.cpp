68f99b05a41c11a17e34695389739b0d17c7c3b7
grzesiekmq <grzesiekfuta@gmail.com>
2023-03-12 12:39:17 +0100

    added green screen

---
+++ r2e.cpp
@@ -0,0 +1,505 @@
+// r2e.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define VULKAN_HPP_NO_CONSTRUCTORS
+#include <iostream>
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <optional>
+#include <vector>
+
+const uint32_t width = 800;
+const uint32_t height = 600;
+    using namespace vk;
+
+    UniqueInstance instance;
+    PhysicalDevice gpu;
+    UniqueDevice device;
+    Queue gfxQueue;
+    Queue presentQueue;
+    SwapchainKHR swapchain;
+    RenderPass rp;
+    std::vector<Image> images;
+    std::vector<ImageView> imageViews;
+    Extent2D extent = { static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
+    std::vector<Framebuffer> framebuffers;
+    SurfaceKHR surface;
+    Pipeline pipeline;
+    PipelineLayout pipelineLayout;
+    CommandPool commandPool;
+    std::vector<CommandBuffer> commandBuffers;
+
+    ImageSubresourceRange imgRange;
+
+    Semaphore imgSemaphore;
+    Semaphore renderSemaphore;
+
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> gfxFamily;
+        std::optional<uint32_t> presentFamily;
+
+        bool isComplete() {
+            return gfxFamily.has_value() && presentFamily.has_value();
+        }
+    };
+
+    const std::vector<const char*> deviceExt = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+    GLFWwindow* window;
+
+    bool createInstance() {
+        uint32_t glfwExtCount = 0;
+        const char** glfwExt;
+
+        glfwExt = glfwGetRequiredInstanceExtensions(&glfwExtCount);
+        std::vector<const char*> ext(glfwExt, glfwExt
+            + glfwExtCount);
+
+        InstanceCreateInfo ci{
+            .pApplicationInfo = {},
+            .enabledExtensionCount = glfwExtCount,
+            .ppEnabledExtensionNames = ext.data()
+
+        };
+        instance = createInstanceUnique(ci);
+
+        std::cout << "instance created" << std::endl;
+
+        return true;
+    }
+
+    bool selectGpu() {
+        auto gpus = instance->enumeratePhysicalDevices();
+
+        gpu = gpus.front();
+
+        std::cout << "gpu selected" << std:: endl;
+        return true;
+    } 
+    
+    QueueFamilyIndices findQueueFamilies(PhysicalDevice device
+    ) {
+        QueueFamilyIndices indices;
+        auto queueFamilies = device.getQueueFamilyProperties();
+
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies) {
+            if (queueFamily.queueCount > 0 && queueFamily.queueFlags & QueueFlagBits::eGraphics) {
+                indices.gfxFamily = i;
+            }
+            if (queueFamily.queueCount > 0 && device.getSurfaceSupportKHR(i, surface)) {
+                indices.presentFamily = i;
+            }
+            if (indices.isComplete()) {
+                break;
+            }
+            i++;
+        }
+        return indices;
+    }
+
+
+
+    bool createDevice() {
+        auto features = PhysicalDeviceFeatures();
+        float priority = 1.0f;
+        DeviceQueueCreateInfo ci{
+            .queueFamilyIndex = 0,
+            .queueCount = 1,
+            .pQueuePriorities = &priority
+        };
+        DeviceCreateInfo deviceCi{
+            .queueCreateInfoCount = 1,
+            .pQueueCreateInfos = &ci,
+            .enabledExtensionCount = static_cast<uint32_t>(deviceExt.size()),
+            .ppEnabledExtensionNames = deviceExt.data(),
+            .pEnabledFeatures = &features
+        };
+
+        device = gpu.createDeviceUnique(deviceCi);
+
+        uint32_t queueGfxFamilyIndex = 0;
+
+        gfxQueue = device->getQueue(findQueueFamilies(gpu).gfxFamily.value(), 0);
+        presentQueue = device->getQueue(findQueueFamilies(gpu).presentFamily.value(), 0);
+        std::cout << "device created" << std::endl;
+        return true;
+    }
+
+    bool createSurface() {
+        VkSurfaceKHR surf;
+
+        glfwCreateWindowSurface((VkInstance)*instance, window, nullptr, &surf);
+
+        surface = static_cast<SurfaceKHR>(surf);
+        std::cout << "surface created" << std::endl;
+        return true;
+    }
+
+    bool createSwapchain() {
+        SurfaceCapabilitiesKHR caps;
+
+        caps = gpu.getSurfaceCapabilitiesKHR(surface);
+
+        SwapchainCreateInfoKHR ci{ .surface = surface,
+        .minImageCount = caps.minImageCount,
+        .imageFormat = Format::eB8G8R8A8Srgb,
+        .imageColorSpace = ColorSpaceKHR::eSrgbNonlinear,
+        .imageExtent = caps.currentExtent,
+        .imageArrayLayers = 1,
+        .imageUsage = ImageUsageFlagBits::eColorAttachment,
+        .imageSharingMode = SharingMode::eExclusive,
+        .presentMode = PresentModeKHR::eFifo,
+        .clipped = true,
+        .oldSwapchain = SwapchainKHR(nullptr) };
+
+        swapchain = device->createSwapchainKHR(ci);
+
+        images = device->getSwapchainImagesKHR(swapchain);
+
+        std::cout << "swapchain created" << std::endl;
+
+        return true;
+    }
+
+    bool createImageViews() {
+        imageViews.resize(images.size());
+
+        for (size_t i = 0;i < images.size(); i++) {
+            ImageViewCreateInfo ci{
+                .image = images[i],
+                .viewType = ImageViewType::e2D,
+                .format = Format::eB8G8R8A8Srgb,
+                .components = {.r = ComponentSwizzle::eIdentity,
+            .g = ComponentSwizzle::eIdentity,
+            .b = ComponentSwizzle::eIdentity,
+            .a = ComponentSwizzle::eIdentity},
+            .subresourceRange = {.aspectMask = ImageAspectFlagBits::eColor,
+            .baseMipLevel = 0,
+            .levelCount = 1,
+            .baseArrayLayer = 0,
+            .layerCount = 1}
+            };
+
+            imageViews[i] = device->createImageView(ci);
+
+        }
+            std::cout << "image views created" << std::endl;
+
+            return true;
+    }
+
+    ShaderModule createShaderModule(const std::vector<char>& code) {
+
+        ShaderModuleCreateInfo ci{ .codeSize = code.size(),
+        .pCode = reinterpret_cast<const uint32_t*>(code.data()) };
+
+        ShaderModule shaderModule;
+        shaderModule = device->createShaderModule(ci);
+
+        std::cout << "shader module created" << std::endl;
+
+        return shaderModule;
+    }
+
+    bool createRenderPass() {
+
+        AttachmentDescription colorAttachment{ .format = Format::eB8G8R8A8Srgb,
+        .samples = SampleCountFlagBits::e1,
+        .loadOp = AttachmentLoadOp::eClear,
+        .storeOp = AttachmentStoreOp::eStore,
+        .stencilLoadOp = AttachmentLoadOp::eDontCare,
+        .stencilStoreOp = AttachmentStoreOp::eDontCare,
+        .initialLayout = ImageLayout::eUndefined,
+        .finalLayout = ImageLayout::ePresentSrcKHR };
+
+        AttachmentReference colorAttachmentRef{ .attachment = 0,
+        .layout = ImageLayout::eColorAttachmentOptimal };
+
+        SubpassDescription subpass{ .pipelineBindPoint = PipelineBindPoint::eGraphics,
+        .colorAttachmentCount = 1,
+        .pColorAttachments = &colorAttachmentRef };
+
+        SubpassDependency dependency{
+            .srcSubpass = VK_SUBPASS_EXTERNAL,
+            .dstSubpass = 0,
+            .srcStageMask = PipelineStageFlagBits::eColorAttachmentOutput,
+            .dstStageMask = PipelineStageFlagBits::eColorAttachmentOutput,
+            .dstAccessMask = AccessFlagBits::eColorAttachmentWrite
+        };
+
+        PipelineLayout pipelineLayout;
+
+        RenderPassCreateInfo ci{
+            .attachmentCount = 1,
+            .pAttachments = &colorAttachment,
+            .subpassCount = 1,
+            .pSubpasses = &subpass,
+            .dependencyCount = 1,
+            .pDependencies = &dependency
+        };
+
+        rp = device->createRenderPass(ci);
+
+        std::cout << "render pass created" << std::endl;
+
+        return true;
+    }
+
+    // todo: read spv
+
+    bool createPipeline() {
+
+
+        PipelineVertexInputStateCreateInfo vi{ .vertexBindingDescriptionCount = 0,
+        .vertexAttributeDescriptionCount = 0 };
+
+        PipelineInputAssemblyStateCreateInfo ia{ .topology = PrimitiveTopology::eTriangleList };
+
+        Viewport vp{};
+        vp.x = vp.y = 0.0f;
+        vp.width = (float)extent.width;
+        vp.height = (float)extent.height;
+        vp.minDepth = 0.0f;
+        vp.maxDepth = 1.0f;
+
+        Rect2D scissor{};
+
+        scissor.extent = extent;
+
+        PipelineViewportStateCreateInfo viewport{
+            .viewportCount = 1,
+            .pViewports = &vp,
+            .scissorCount = 1,
+            .pScissors = &scissor
+        };
+
+        PipelineRasterizationStateCreateInfo rs{
+            .polygonMode = PolygonMode::eFill,
+            .cullMode = CullModeFlagBits::eBack,
+            .frontFace = FrontFace::eClockwise,
+            .lineWidth = 1.0f
+        };
+
+        PipelineLayoutCreateInfo pipeLayoutCi{};
+
+        pipelineLayout = device->createPipelineLayout(pipeLayoutCi);
+
+        GraphicsPipelineCreateInfo pipelineCi{
+            .stageCount = 2,
+            .pStages = nullptr,
+            .pVertexInputState = &vi,
+            .pInputAssemblyState = &ia,
+            .pViewportState = &viewport,
+            .pRasterizationState = &rs,
+            .layout = pipelineLayout,
+            .renderPass = rp
+        };
+
+        Result result;
+
+        std::tie(result, pipeline) = device->createGraphicsPipeline(nullptr, pipelineCi);
+
+        std::cout << "pipeline created" << std::endl;
+
+        return true;
+
+    }
+
+    bool createFramebuffers() {
+        framebuffers.resize(imageViews.size());
+
+        for (size_t i = 0;i < imageViews.size(); i++) {
+            ImageView attachments[] = {
+                imageViews[i]
+            };
+
+            FramebufferCreateInfo ci{
+                .renderPass = rp,
+                .attachmentCount = 1,
+                .pAttachments = attachments,
+                .width = extent.width,
+                .height = extent.height,
+                .layers = 1
+            };
+
+            framebuffers[i] = device->createFramebuffer(ci);
+        }
+
+        std::cout << "framebuffers created" << std::endl;
+
+        return true;
+    }
+
+    bool createCommandPool() {
+        CommandPoolCreateInfo ci{ .queueFamilyIndex = 0,
+        };
+
+        commandPool = device->createCommandPool(ci);
+
+        std::cout << "command pool created" << std::endl;
+
+        return true;
+    }
+
+    bool createCommandBuffers() {
+        commandBuffers.resize(framebuffers.size());
+
+        CommandBufferAllocateInfo ci{ .commandPool = commandPool,
+        .level = CommandBufferLevel::ePrimary,
+        .commandBufferCount = static_cast<uint32_t>(commandBuffers.size()) };
+
+        commandBuffers = device->allocateCommandBuffers(ci);
+
+        for (size_t i = 0;i < commandBuffers.size(); i++) {
+            ClearValue clearColor = { std::array<float,4>{0.0f, 1.0f, 0.0f, 1.0f} };
+
+            CommandBufferBeginInfo info{};
+
+            commandBuffers[i].begin(info);
+
+            RenderPassBeginInfo rpInfo{
+                .renderPass = rp,
+                .framebuffer = framebuffers[i],
+                .renderArea = {.offset = {0, 0}, .extent = extent},
+                .clearValueCount = 1,
+                .pClearValues = &clearColor };
+
+            commandBuffers[i].beginRenderPass(rpInfo, SubpassContents::eInline);
+
+            commandBuffers[i].bindPipeline(PipelineBindPoint::eGraphics, pipeline);
+
+            //commandBuffers[i].draw(3, 1, 0, 0);
+
+            commandBuffers[i].endRenderPass();
+            commandBuffers[i].end();
+        }
+
+        std::cout << "command buffers allocated" << std::endl;
+
+        return true;
+    }
+
+    bool createSemaphores() {
+
+        SemaphoreCreateInfo ci{};
+
+        imgSemaphore = device->createSemaphore(ci);
+        renderSemaphore = device->createSemaphore(ci);
+
+        std::cout << "semaphores created" << std::endl;
+
+        return true;
+    }
+
+    void render() {
+        uint32_t imgIndex;
+
+        imgIndex = device->acquireNextImageKHR(swapchain, UINT64_MAX, imgSemaphore, nullptr).value;
+
+        Semaphore waitSemaphores[] = { imgSemaphore };
+        Semaphore signalSemaphores[] = { renderSemaphore };
+
+        PipelineStageFlags waitDstStages[] = { PipelineStageFlagBits::eColorAttachmentOutput };
+
+        SwapchainKHR swapchains[] = { swapchain };
+
+        SubmitInfo info{ .waitSemaphoreCount = 1,
+        .pWaitSemaphores = waitSemaphores,
+        .pWaitDstStageMask = waitDstStages,
+        .commandBufferCount = 1,
+        .pCommandBuffers = &commandBuffers[imgIndex],
+        .signalSemaphoreCount = 1,
+        .pSignalSemaphores = signalSemaphores };
+
+        gfxQueue.submit(info);
+
+        PresentInfoKHR presentInfo{ .waitSemaphoreCount = 1,
+        .pWaitSemaphores = signalSemaphores,
+        .swapchainCount = 1,
+        .pSwapchains = swapchains,
+        .pImageIndices = &imgIndex };
+
+        presentQueue.presentKHR(presentInfo);
+
+
+
+    }
+
+    void cleanup() {
+        device->destroySemaphore(renderSemaphore);
+        device->destroySemaphore(imgSemaphore);
+
+
+        device->freeCommandBuffers(commandPool, commandBuffers);
+
+        device->destroyCommandPool(commandPool);
+
+        for (auto& framebuffer : framebuffers) {
+            device->destroyFramebuffer(framebuffer);
+        }
+
+        device->destroyPipeline(pipeline);
+        device->destroyPipelineLayout(pipelineLayout);
+        device->destroyRenderPass(rp);
+
+        for (auto& imageView : imageViews) {
+            device->destroyImageView(imageView);
+        }
+
+        device->destroySwapchainKHR(swapchain);
+
+        instance->destroySurfaceKHR(surface);
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+            render();
+
+        }
+        device->waitIdle();
+    }
+
+    void init() {
+        createInstance();
+        createSurface();
+        selectGpu();
+        createDevice();
+
+        createSwapchain();
+        createImageViews();
+        createRenderPass();
+        createPipeline();
+        createFramebuffers();
+        createCommandPool();
+        createCommandBuffers();
+
+        createSemaphores();
+    }
+
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    window = glfwCreateWindow(width, height, "Vulkan triangle", nullptr, nullptr);
+
+    init();
+  
+    mainLoop();
+
+    cleanup();
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
+
+
+ 
